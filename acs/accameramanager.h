f374013ea89a54ea9c856deb22d4d129fbfe9033
Дмитрий <dmitry@enula.ru>
2022-02-21 02:10:19 +0300

    gameobject

--- acs/accameramanager.h
+++ acs/accameramanager.h
@@ -2,20 +2,52 @@
 
 class ACCameraManager
 {
-void ACCameraManager::ACCameraManager(Sim *isim, CameraForward *icamera);
-void ACCameraManager::~ACCameraManager();
-ACCameraManager *ACCameraManager::`vector deleting destructor'(unsigned int a2);
-void ACCameraManager::fadeIn(FadeModeDef aFadeMode);
-bool ACCameraManager::isCameraOnBoard(CarAvatar *car);
-bool ACCameraManager::isVirtualMirrorOn();
-void ACCameraManager::loadPreviousSessionUserCamera();
-void ACCameraManager::registerToFadeInEvent(std::function<void __cdecl(bool const &)> f, void *sender);
-void ACCameraManager::setAudioDistanceScale();
-void ACCameraManager::setCameraCarIndex(int index);
-void ACCameraManager::setDrivableCarIndex(int index);
-void ACCameraManager::setKeyboardInputEnabled(bool value);
-void ACCameraManager::setMode(CameraMode imode, bool force, bool pIsRandom);
-void ACCameraManager::stepCameraRandom(int lastCarIndex);
-void ACCameraManager::update(float deltaT);
+public:
+    void ACCameraManager::ACCameraManager(Sim *isim, CameraForward *icamera);
+    void ACCameraManager::~ACCameraManager();
 
+    void ACCameraManager::fadeIn(FadeModeDef aFadeMode);
+    bool ACCameraManager::isCameraOnBoard(CarAvatar *car);
+    bool ACCameraManager::isVirtualMirrorOn();
+    void ACCameraManager::loadPreviousSessionUserCamera();
+    void ACCameraManager::registerToFadeInEvent(std::function<void __cdecl(bool const &)> f, void *sender);
+    void ACCameraManager::setAudioDistanceScale();
+    void ACCameraManager::setCameraCarIndex(int index);
+    void ACCameraManager::setDrivableCarIndex(int index);
+    void ACCameraManager::setKeyboardInputEnabled(bool value);
+    void ACCameraManager::setMode(CameraMode imode, bool force, bool pIsRandom);
+    void ACCameraManager::stepCameraRandom(int lastCarIndex);
+    void ACCameraManager::update(float deltaT);
+
+private:
+    Event<int> evOnCameraSelectedIndex;
+    Event<bool> evOnCameraUpdateDone;
+    CameraOnBoard *cameraOnBoard;
+    CameraMouseControl *mouseControl;
+    CinematicFreeCamera *cinematicMouseControl;
+    CameraTrack *cameraTrack;
+    CameraHelicopter *cameraHelicopter;
+    CameraOnBoardFree *cameraOnBoardFree;
+    CameraCarManager *cameraCar;
+    CameraDrivableManager *cameraDrivable;
+    CameraModeStart *cameraStart;
+    ImageGeneratorCamera *imageGeneratorCamera;
+    bool isKeyboardCameraControlEnabled;
+    int currentGlobalCameraIndex;
+    float randomCameraTime;
+    float randomOldCameraTime;
+    float randomCameraMinTime;
+    float randomCameraMaxTime;
+    std::vector<int> randomCameraProbabilities;
+    bool randomMode;
+    CameraMode lastRandomCamera;
+    Sim *sim;
+    CameraForward *camera;
+    CameraMode mode;
+    CameraMode lastDrivingMode;
+    FadeModeDef fadeMode;
+    Texture fadeTexture;
+    vec3f fadeColor;
+    Event<bool> evOnFadeInFinished;
+    PersistanceCameraModeDef persistanceCameraMode;
 };
--- ksgame/gameobject.h
+++ ksgame/gameobject.h
@@ -1,42 +1,35 @@
 #pragma once
 
+#include <vector>
+#include <string>
+
 class GameObject
 {
-TimeLimitedTest *GameObject::getGameObject<TimeLimitedTest>();
-TrackAvatar *GameObject::getGameObject<TrackAvatar>();
-std::vector<CarAvatar *> *GameObject::getGameObjects<CarAvatar>(std::vector<CarAvatar *> *result);
-Sim *GameObject::getGameObject<Sim>();
-PhysicsAvatar *GameObject::getGameObject<PhysicsAvatar>();
-PushToPassManager *GameObject::getGameObject<PushToPassManager>();
-ACClient *GameObject::getGameObject<ACClient>();
-GameObject *GameObject::getGameObject(std::wstring name);
-SunAnimator *GameObject::getGameObject<SunAnimator>();
-SteamInterface *GameObject::getGameObject<SteamInterface>();
-GameMode *GameObject::getGameObject<GameMode>();
-PostProcessEffectsUpdater *GameObject::getGameObject<PostProcessEffectsUpdater>();
-StartingLights *GameObject::getGameObject<StartingLights>();
-ACCameraManager *GameObject::getGameObject<ACCameraManager>();
-StartingTrackSemaphore *GameObject::getGameObject<StartingTrackSemaphore>();
-PitStop *GameObject::getGameObject<PitStop>();
-PitCrew *GameObject::addGameObject<ProximityIndicator>(PitCrew *object);
-QuickMenuPitstop *GameObject::getGameObject<QuickMenuPitstop>();
-FlagManager *GameObject::getGameObject<FlagManager>();
-CarAvatar *GameObject::getGameObject<CarAvatar>();
-GhostCar *GameObject::getGameObject<GhostCar>();
-RaceManager *GameObject::getGameObject<RaceManager>();
-DragMode *GameObject::getGameObject<DragMode>();
-DriftMode *GameObject::getGameObject<DriftMode>();
-TimeAttack *GameObject::getGameObject<TimeAttack>();
-SplashScreen *GameObject::getGameObject<SplashScreen>();
-WeatherManager *GameObject::getGameObject<WeatherManager>();
-CarBrakeLights *GameObject::getGameObject<CarBrakeLights>();
-DynamicCarEffects *GameObject::getGameObject<DynamicCarEffects>();
-ACOperatingSystem *GameObject::addGameObject<ACOperatingSystem>(ACOperatingSystem *object);
-DamageDisplayer *GameObject::getGameObject<DamageDisplayer>();
-DriverNameDisplayer *GameObject::getGameObject<DriverNameDisplayer>();
-MouseHider *GameObject::getGameObject<MouseHider>();
-void GameObject::GameObject(std::wstring iname, Game *igame);
-void GameObject::~GameObject();
-GameObject *GameObject::getParent();
+    GameObject(std::wstring iname, Game *igame);
 
+    virtual void update(float) = 0;
+    virtual void render(float) = 0;
+    virtual void renderHUD(float) = 0;
+    virtual void renderAudio(float) = 0;
+    virtual void shutdown() = 0;
+    virtual ~GameObject() = 0;
+
+    GameObject *GameObject::getParent();
+    GameObject *getGameObject(std::wstring name);
+
+    template <typename T>
+    T *getGameObject();
+
+    template <typename T>
+    std::vector<T *> *getGameObjects(std::vector<T *> *result);
+
+    template <typename T>
+    T *addGameObject(T *object);
+
+private:
+    Game *game;
+    std::wstring name;
+    bool isActive;
+    GameObject *parent;
+    std::vector<GameObject *> gameObjects;
 };
